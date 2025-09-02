34f6a8d7b9f4edaab3dd0de460e135b8022b5f1e
ruamess <zlaptiev@gmail.com>
2025-09-02 20:41:18 +0500

    add lab 1

---
+++ labworks.js
@@ -0,0 +1,18 @@
+// lab 1
+// arithmetic exPression
+
+5 + 5 // => 10
+8 / 2 // => 4
+
+let a = 3
+let b = 4
+let c = 5
+
+let P = a + b + c
+
+console.log(`Perimeter of the triangle is: ${P}`)
+
+P += 1
+P /= 2
+P -= 3
+P *= 4
