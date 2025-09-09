863d014db0c45062779affcdae79d29e801a52b1
ruamess <zlaptiev@gmail.com>
2025-09-09 13:09:55 +0500

    rewrite 1 lab

--- labworks.js
+++ labworks.js
@@ -1,18 +1,20 @@
 // lab 1
-// arithmetic exPression
+// arithmetic expressions
+a = 3
+b = 4
+c = 5
 
-5 + 5 // => 10
-8 / 2 // => 4
-
-let a = 3
-let b = 4
-let c = 5
-
-let P = a + b + c
-
+P = a + b + c
 console.log(`Perimeter of the triangle is: ${P}`)
 
 P += 1
+console.log(`After P += 1: ${P}`)
+
 P /= 2
+console.log(`After P /= 2: ${P}`)
+
 P -= 3
+console.log(`After P -= 3: ${P}`)
+
 P *= 4
+console.log(`After P *= 4: ${P}`)
